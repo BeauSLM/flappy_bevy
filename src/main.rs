0e173e1260a0f2e0f9456cb8f47abbca4877a4a4
Beau McCartney <mccartney.beausl@gmail.com>
2022-03-22 22:46:09 -0600

    Load in bird sprite and add basic movement

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,38 @@
+use bevy::prelude::*;
+
+#[derive(Component)]
+struct Bird {
+    speed: f32,
+}
+
 fn main() {
-    println!("Hello, world!");
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_startup_system(setup)
+        .add_system(bird_movement)
+        .run();
+}
+
+fn setup(mut commands: Commands, assets: Res<AssetServer>) {
+    commands.spawn_bundle(OrthographicCameraBundle::new_2d());
+    commands.spawn_bundle(SpriteBundle {
+        texture: assets.load("sprites/bluebird-midflap.png"),
+        ..Default::default()
+    })
+    .insert(Bird { speed: 0. });
+}
+
+fn bird_movement(
+    mut bird: Query<(&mut Bird, &mut Transform)>,
+    keyboard: Res<Input<KeyCode>>,
+    ) {
+    const GRAVITY: f32 = 0.15;
+    const FLAP: f32 = 3.;
+    let (mut bird, mut trans) = bird.single_mut();
+    if keyboard.pressed(KeyCode::Space) {
+        bird.speed = FLAP;
+    }
+    trans.translation.y += bird.speed;
+    bird.speed -= GRAVITY;
+    // terminal velocity?
 }
