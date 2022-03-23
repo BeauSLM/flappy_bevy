802562fe93734954d9688e67ecb2ec19937777c0
Beau McCartney <mccartney.beausl@gmail.com>
2022-03-23 15:02:48 -0600

    Load all sprites in initial loading state

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,22 @@
-use bevy::prelude::*;
+use bevy::{
+    prelude::*,
+    asset::LoadState,
+};
 
 #[derive(Component)]
 struct Bird {
     speed: f32,
+}
+
+#[derive(Debug, Clone, PartialEq, Eq, Hash)]
+enum AppState {
+    Setup,
+    Finished,
+}
+
+#[derive(Default)]
+struct SpriteHandles {
+    handles: Vec<HandleUntyped>,
 }
 
 fn main() {
@@ -13,21 +27,40 @@ fn main() {
             height: 512.,
             ..Default::default()
         })
+        .init_resource::<SpriteHandles>()
+        .add_state(AppState::Setup)
         .add_plugins(DefaultPlugins)
-        .add_startup_system(setup)
-        .add_system(bird_movement)
+        .add_system_set(SystemSet::on_enter(AppState::Setup).with_system(load_sprites))
+        .add_system_set(SystemSet::on_update(AppState::Setup).with_system(check_loading))
+        .add_system_set(SystemSet::on_enter(AppState::Finished).with_system(setup))
+        .add_system_set(SystemSet::on_update(AppState::Finished)
+                        .with_system(bird_movement))
         .run();
+}
+
+fn load_sprites(mut sprite_handles: ResMut<SpriteHandles>, assets: Res<AssetServer>) {
+    sprite_handles.handles = assets.load_folder("sprites").unwrap();
+}
+
+fn check_loading(
+    mut state: ResMut<State<AppState>>,
+    sprites: ResMut<SpriteHandles>,
+    assets: Res<AssetServer>,
+    ) {
+    if let LoadState::Loaded = assets.get_group_load_state(sprites.handles.iter().map(|handle| handle.id)) {
+        state.set(AppState::Finished).unwrap();
+    }
 }
 
 fn setup(mut commands: Commands, assets: Res<AssetServer>) {
     commands.spawn_bundle(OrthographicCameraBundle::new_2d());
     commands.spawn_bundle(SpriteBundle {
-        texture: assets.load("sprites/bluebird-midflap.png"),
+        texture: assets.get_handle("sprites/bluebird-midflap.png"),
         ..Default::default()
     })
     .insert(Bird { speed: 0. });
     commands.spawn_bundle(SpriteBundle {
-        texture: assets.load("sprites/background-day.png"),
+        texture: assets.get_handle("sprites/background-day.png"),
         ..Default::default()
     });
 }
