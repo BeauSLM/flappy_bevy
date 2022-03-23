9d70ef2458b47baa64d427e56b2ff7954c22726d
Beau McCartney <mccartney.beausl@gmail.com>
2022-03-23 16:51:20 -0600

    Animate the bird - lord please forgive me

--- src/main.rs
+++ src/main.rs
@@ -34,7 +34,8 @@ fn main() {
         .add_system_set(SystemSet::on_update(AppState::Setup).with_system(check_loading))
         .add_system_set(SystemSet::on_enter(AppState::Finished).with_system(setup))
         .add_system_set(SystemSet::on_update(AppState::Finished)
-                        .with_system(bird_movement))
+                        .with_system(bird_movement)
+                        .with_system(bird_animation))
         .run();
 }
 
@@ -52,17 +53,64 @@ fn check_loading(
     }
 }
 
-fn setup(mut commands: Commands, assets: Res<AssetServer>) {
+fn setup(
+    mut commands: Commands,
+    assets: Res<AssetServer>,
+    mut atlases: ResMut<Assets<TextureAtlas>>,
+    mut textures: ResMut<Assets<Image>>,
+    ) {
+    const BIRD_SPRITES: [&str; 3] = [
+        "sprites/bluebird-upflap.png",
+        "sprites/bluebird-midflap.png",
+        "sprites/bluebird-downflap.png",
+    ];
+    let mut atlas_builder = TextureAtlasBuilder::default();
+    for path in BIRD_SPRITES {
+        let handle = assets.get_handle(path);
+        let texture = textures.get(handle.clone_weak()).unwrap();
+        atlas_builder.add_texture(handle, texture);
+    }
+    let atlas = atlas_builder.finish(&mut textures).unwrap();
+    let _atlas_texture = atlas.texture.clone();
+    let atlas_handle = atlases.add(atlas);
     commands.spawn_bundle(OrthographicCameraBundle::new_2d());
-    commands.spawn_bundle(SpriteBundle {
-        texture: assets.get_handle("sprites/bluebird-midflap.png"),
+    let bird_transform = Transform {
+        translation: Vec2::ZERO.extend(1.),
+        ..Default::default()
+    };
+    // show spritesheet
+    // commands.spawn_bundle(SpriteBundle {
+    //     texture: _atlas_texture,
+    //     transform: bird_transform,
+    //     ..Default::default()
+    // });
+    commands.spawn_bundle(SpriteSheetBundle {
+        transform: bird_transform,
+        texture_atlas: atlas_handle,
         ..Default::default()
     })
-    .insert(Bird { speed: 0. });
+    .insert(Bird { speed: 0. })
+    .insert(Timer::from_seconds(0.1, true));
     commands.spawn_bundle(SpriteBundle {
         texture: assets.get_handle("sprites/background-day.png"),
         ..Default::default()
     });
+}
+
+fn bird_animation(
+    time: Res<Time>,
+    mut query: Query<(&mut Timer, &mut TextureAtlasSprite)>,
+    // XXX: I think I'm condemmed for this
+    mut flap_ix_ix: Local<usize>,
+    ) {
+    static FLAP_IX: [usize; 4] = [2, 1, 2, 0];
+    let (mut timer, mut sprite) = query.single_mut();
+    timer.tick(time.delta());
+    if timer.finished() {
+        // XXX: LISTEN MAN IT MAKES THE BIRD FLAP RIGHT I'M SORRY
+        sprite.index = FLAP_IX[*flap_ix_ix];
+        *flap_ix_ix = (*flap_ix_ix + 1) % FLAP_IX.len();
+    }
 }
 
 fn bird_movement(
